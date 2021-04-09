791e2cc56ccaccfa28270aa73d1b0412f14630ba
Nadno <eunadno@gmail.com>
2021-04-09 20:01:53 -0300

    adjust project settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,58 @@
-const path = require("path");
+const path = require('path');
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", "index.ts"),
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  devtool: "source-map",
-  mode: "production",
-  
+  devtool: 'source-map',
+  mode: 'production',
+
   resolve: {
-    extensions: [".webpack.js", ".web.js", ".ts", ".js"],
+    extensions: ['.webpack.js', '.web.js', '.ts', '.js'],
   },
 
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
-      { test: /\.js$/, exclude: /node_modules/, loader: "source-map-loader" },
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
+        loader: 'source-map-loader',
+        enforce: 'pre',
         exclude: /node_modules/,
-        loader: "babel-loader",
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+            plugins: [['@babel/plugin-proposal-class-properties']],
+          },
+        },
       },
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "public", "index.html"),
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public', 'index.html'),
     }),
     new MiniCssExtractPlugin({
-      filename: "styles.css",
+      filename: 'styles.css',
     }),
   ],
 };
