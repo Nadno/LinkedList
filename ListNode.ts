e08c07d63050c87eca07ee6832e89026baec5ecc
Nadno <eunadno@gmail.com>
2021-06-26 14:00:37 -0300

    refactor(ListNode): use undefined values instead of null

--- ListNode.ts
+++ ListNode.ts
@@ -1,49 +1,49 @@
 import { IListNode } from './types';
 
 export default class ListNode<NodeType = any> implements IListNode<NodeType> {
-  constructor(
-    public value: NodeType,
-    public next: IListNode<NodeType> | null = null,
-    public prev: IListNode<NodeType> | null = null
-  ) {}
+	constructor(
+		public value: NodeType,
+		public next?: IListNode<NodeType>,
+		public prev?: IListNode<NodeType>
+	) {}
 
-  public insertNext(
-    value: NodeType,
-    afterNewNode: IListNode<NodeType> | null = null
-  ): IListNode<NodeType> {
-    const node: IListNode<NodeType> = new ListNode(value, afterNewNode, this);
-    if (afterNewNode) {
-      afterNewNode.prev = node;
-    }
+	public insertNext(
+		value: NodeType,
+		afterNewNode: IListNode<NodeType>
+	): IListNode<NodeType> {
+		const node: IListNode<NodeType> = new ListNode(value, afterNewNode, this);
+		if (afterNewNode) {
+			afterNewNode.prev = node;
+		}
 
-    this.next = node;
-    return node;
-  }
+		this.next = node;
+		return node;
+	}
 
-  public insertPrevious(
-    value: NodeType,
-    beforeNewNode: IListNode<NodeType> | null = null
-  ): IListNode<NodeType> {
-    const node = new ListNode(value, this, beforeNewNode);
-    if (beforeNewNode) {
-      beforeNewNode.next = node;
-    }
+	public insertPrevious(
+		value: NodeType,
+		beforeNewNode: IListNode<NodeType>
+	): IListNode<NodeType> {
+		const node = new ListNode(value, this, beforeNewNode);
+		if (beforeNewNode) {
+			beforeNewNode.next = node;
+		}
 
-    this.prev = node;
-    return node;
-  }
+		this.prev = node;
+		return node;
+	}
 
-  public remove(): IListNode<NodeType> {
-    const { next, prev } = this;
+	public remove(): IListNode<NodeType> {
+		const { next, prev } = this;
 
-    if (prev) {
-      prev.next = next;
-    }
+		if (prev) {
+			prev.next = next;
+		}
 
-    if (next) {
-      next.prev = prev;
-    }
+		if (next) {
+			next.prev = prev;
+		}
 
-    return this;
-  }
+		return this;
+	}
 }
